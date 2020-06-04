30a0253bdeb4b93b068265423b69016d69e93b80
2015004302 <kwaksw@naver.com>
2020-06-04 19:16:12 +0900

    temp

--- ClassAssignment3/main.py
+++ ClassAssignment3/main.py
@@ -16,6 +16,11 @@ scale = 1.
 trans = np.array([0.,0.,0.])
 t1 = 0.
 full_list = []
+START_FLAG = False
+tree = []
+motion_start = 0 
+sum_channel = 0
+cur_index = 0
 
 class Node:
     def __init__(self,name):
@@ -25,16 +30,15 @@ class Node:
         self.channel_count = 0
         self.channel = []
         self.child = []
-        self.parent = ''
+        self.parent_index = -1
     
 # Make tree structure for parsing hierarchical model
-tree = []
-motion_start = 0 
 def make_tree():
-    global full_list, tree, motion_start
+    global full_list, tree, motion_start, sum_channel, cur_index
     tree = []
     motion_start = 0
     i = 0
+    sum_channel = 0
     pre_index = -1
     while True:
         temp = full_list[i].split(' ')
@@ -45,9 +49,9 @@ def make_tree():
             # parent, child add
             if temp[0] == "JOINT":
                 tree[pre_index].child.append(temp_Node)
-                temp_Node.parent = tree[pre_index]
-            elif temp[0] == "ROOT":
-                temp_Node.parent = None
+                temp_Node.parent_index = pre_index
+            # elif temp[0] == "ROOT":
+            #     temp_Node.parent = None
             
             # offset
             temp1 = full_list[i+2].split(' ')
@@ -56,65 +60,83 @@ def make_tree():
             # channel
             temp2 = full_list[i+3].split(' ')
             temp_Node.channel_count = int(temp2[1])
+            sum_channel += temp_Node.channel_count
+            # 기본적으로 x,y,z position channel이 root에 있다고 가정
+            if temp[0] == "ROOT":
+                temp_Node.channel_count -= 3
             for j in range(temp_Node.channel_count):
+                if(temp[0] == "ROOT"):
+                    j += 3
                 if temp2[2+j] == "XROTATION":
                     temp_Node.channel.append("XROT")
                 elif temp[2+j] == "YROTATION":
                     temp_Node.channel.append("YROT")
                 elif temp[2+j] == "ZROTATION":
                     temp_Node.channel.append("ZROT")
-                elif temp[2+j] == "XPOSITION":
-                    temp_Node.channel.append("XPOS")
-                elif temp[2+j] == "YPOSITION":
-                    temp_Node.channel.append("YPOS")
-                elif temp[2+j] == "ZPOSITION":
-                    temp_Node.channel.append("ZPOS")
+                # elif temp[2+j] == "XPOSITION":
+                #     temp_Node.channel.append("XPOS")
+                # elif temp[2+j] == "YPOSITION":
+                #     temp_Node.channel.append("YPOS")
+                # elif temp[2+j] == "ZPOSITION":
+                #     temp_Node.channel.append("ZPOS")
             # tree index
-            pre_index += 1
+            pre_index = len(tree) - 1
             temp_Node.tree_index = pre_index
             i += 4
         elif temp[0] == "End":
             temp_Node = Node("__END__")
             tree[pre_index].child.append(temp_Node)
-            temp_Node.parent = tree[pre_index]
+            temp_Node.parent_index = pre_index
             # offset
             temp1 = full_list[i+2].split(' ')
             temp_Node.offset = np.array([int(temp1[1]),int(temp1[2]),int(temp1[3])])
             # tree index
-            pre_index += 1
+            pre_index = len(tree) - 1
             temp_Node.tree_index = pre_index
             i += 3
         elif temp[0] == '}':
-            pre_index -= 1
+            pre_index = tree[pre_index].parent_index
             i += 1
         elif temp[0] == "MOTION":
             motion_start = i
             break
+    cur_index = motion_start + 3
 
-def Euler_X(angle):
-    return np.array([])
-def draw_Model(node,motion_data,motion_index):
+motion_index = 0
+def draw_Model(node,motion_data):
+    global motion_index
+    # end site
+    glPushMatrix()
     if node.channel_count == 0:
-        glPushMatrix()
         glBegin(GL_LINES)
         glVertex3fv(0.,0.,0.)
         glTranslatef(node.offset[0],node.offset[1],node.offset[2])
         glVertex3fv(0.,0.,0.)
-        glPopMatrix()
+        glEnd()
+    # joint & root
     else:
-        M = np.zeros((4,4))
-        glPushMatrix()
+        if node.tree_index == 0:
+            glTranslatef(motion_data[0],motion_data[1],motion_data[2])
+            motion_index = 3
         glBegin(GL_LINES)
         glVertex3fv(0.,0.,0.)
         glTranslatef(node.offset[0],node.offset[1],node.offset[2])
         for i in node.channel:
             if i == "XROT":
-                
-        
-        
-        
-    
+                glRotatef(np.rad2deg(motion_data[motion_index]), 1, 0, 0)
+            elif i == "YROT":
+                glRotatef(np.rad2deg(motion_data[motion_index]), 0, 1, 0)
+            elif i == "ZROT":
+                glRotatef(np.rad2deg(motion_data[motion_index]), 0, 0, 1)
+            motion_index += 1
+        glVertex3fv(0.,0.,0.)
+        glEnd()
+        for child in node.child:
+            draw_Model(child,motion_data)
+    glPopMatrix()
+
 def render():
+    global full_list, START_FLAG, motion_start, tree, sum_channel, cur_index, motion_index
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glEnable(GL_DEPTH_TEST)
     glPolygonMode(GL_FRONT_AND_BACK,GL_FILL)
@@ -131,7 +153,19 @@ def render():
     
     drawframe()
     drawgrid()
-    # drawModel()
+    motion_data = []
+    if START_FLAG:
+        motion_data = full_list[cur_index].split(' ')
+        if not motion_data:
+            cur_index = motion_start + 3
+            motion_data = full_list[cur_index].split(' ')
+        for i in range(sum_channel):
+            motion_data[i] = float(motion_data[i])
+        cur_index += 1
+    else:
+        motion_data = np.zeros(sum_channel)
+    glColor3ub(0,0,200)
+    draw_Model(tree[0], motion_data)
 
 # draw grid
 def drawgrid():
@@ -219,6 +253,12 @@ def drop_callback(window,paths):
     
     file.close()
 
+def key_callback(window, key, scancode, action, mods):
+    global START_FLAG
+    if action==glfw.PRESS or action == glfw.REPEAT:
+        if key == glfw.KEY_SPACE:
+            START_FLAG = True
+
 def main():
     global t1
     if not glfw.init():
@@ -234,6 +274,7 @@ def main():
     glfw.set_mouse_button_callback(window,button_callback)
     glfw.set_scroll_callback(window,scroll_callback)
     glfw.set_drop_callback(window,drop_callback)
+    glfw.set_key_callback(window,key_callback)
     glfw.swap_interval(1)
     while not glfw.window_should_close(window):
         glfw.poll_events()
---
+++ LabAssignment4/example.py
@@ -0,0 +1,5 @@
+sum = 0
+for i range(3):
+    i +=2
+    sum += i
+print(sum)
